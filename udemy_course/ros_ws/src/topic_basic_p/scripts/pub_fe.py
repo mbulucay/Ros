374a60f51fc1ea727eca1272565fbd112422d121
mbulucay <ulucaybedir@gmail.com>
2022-02-20 17:07:52 +0300

    launch basic

--- udemy_course/ros_ws/src/topic_basic_p/scripts/pub_fe.py
+++ udemy_course/ros_ws/src/topic_basic_p/scripts/pub_fe.py
@@ -3,7 +3,7 @@ import rospy
 from std_msgs.msg import String
 
 rospy.init_node("pub_n")
-publisher = rospy.Publisher("top_name_1", data_class = String, queue_size = 10)
+publisher = rospy.Publisher("top_new_1", data_class = String, queue_size = 10)
 msg = String("Neler neler oluyor")
 r = rospy.Rate(10)
 
